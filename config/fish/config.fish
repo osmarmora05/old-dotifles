5bc7631cee7a86fe4dd0dfde5f0420dee67b029b
osmarmora05 <osmaradrianmoracerna@gmail.com>
2023-09-06 16:37:05 -0600

    refactor: gruvbox_dark background in Kitty

--- config/fish/config.fish
+++
@@ -1,7 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    # colorscript random
-    # pokemon-colorscripts -r
-    pokemon-colorscripts -r --no-title
-    starship init fish | source	
-end
--- config/fish/functions/img.fish
+++
@@ -1,4 +0,0 @@
-function img --wraps='kitty +kitten icat' --description 'alias img=kitty +kitten icat'
-  kitty +kitten icat $argv
-        
-end
