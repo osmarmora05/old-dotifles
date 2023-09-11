a62fa2c24fb20b7cb17956adb6ae08d641d7a94b
osmarmora05 <osmaradrianmoracerna@gmail.com>
2023-09-11 15:31:58 -0600

    feat: ui animations & widgets with rubato

--- config/awesome/widgets/controlCenter/init.lua
+++ config/awesome/widgets/controlCenter/init.lua
@@ -5,6 +5,7 @@ local dpi       = beautiful.xresources.apply_dpi
 
 local helpers   = require('helpers')
 local widgets   = require('widgets.controlCenter.module')
+local rubato    = require('modules.rubato')
 
 
 local panel = wibox{
@@ -12,7 +13,7 @@ local panel = wibox{
    visible = false,
    width   = dpi(410),
    height  = dpi(640),
-   y       = dpi(55),
+   --y       = dpi(55),
    x       = dpi(8),
    bg      = beautiful.bg_normal,
    shape   = helpers.rounded_rect(dpi(4)),
@@ -42,14 +43,52 @@ local panel = wibox{
     widget = wibox.container.margin
 
 }
+
 }
 
+-- Animations
+local slide = rubato.timed{
+    pos = dpi(-panel.height),
+    rate = 60,
+    intro = 0.1,
+    duration = 0.46,
+    awestore_compat = true,
+    subscribed = function(pos) panel.y = pos end
+}
+
+-- Flag
+local controlCenter_status = false
+
+slide.ended:subscribe(function()
+    if controlCenter_status then
+        panel.visible = false
+    end
+end)
+
+local controlCenter_show = function()
+    slide:set(dpi(55) + beautiful.useless_gap * 2)
+    panel.visible = true
+    controlCenter_status = false
+end
+
+local controlCenter_hide = function()
+    slide:set(dpi(-panel.height))
+    controlCenter_status = true
+end
+
 function panel:show()
-    panel.visible = not panel.visible
+    if panel.visible then
+        controlCenter_hide()
+    else
+        controlCenter_show()
+    end
+
+    -- panel.visible = not panel.visible
 
     if panel.visible then
         widgets.themer.reset()
     end
  end
- 
+
+
 return panel
