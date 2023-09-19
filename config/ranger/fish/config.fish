77d8de3dd41b607e04bbe540b3ec6a3617de2ca8
osmarmora05 <osmaradrianmoracerna@gmail.com>
2023-09-19 10:04:08 -0600

    chore: deleted kitty, rofi, fish in ranger config

--- config/ranger/fish/config.fish
+++
@@ -1,6 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    # colorscript random
-    # pokemon-colorscripts -r
-    pokemon-colorscripts -r --no-title	
-end
