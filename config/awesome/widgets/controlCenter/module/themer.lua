cad5860351edfa59ec40af2718d0cb7c4d2fb361
osmarmora05 <osmaradrianmoracerna@gmail.com>
2023-09-20 16:05:05 -0600

    feat: add gradient background in themer widget

--- config/awesome/widgets/controlCenter/module/themer.lua
+++ config/awesome/widgets/controlCenter/module/themer.lua
@@ -1,17 +1,17 @@
--- by
+-- by chadcat7
 
 local wibox       = require("wibox")
 local awful       = require("awful")
 local beautiful   = require("beautiful")
-local dpi       = beautiful.xresources.apply_dpi
 local helpers     = require("helpers")
 local gears       = require("gears")
 local user        = require('config.user')
 local gfs         = gears.filesystem
 
-
-
 local preview_image = gfs.get_configuration_dir() .. 'theme/preview/'
+
+
+local currTheme   = user.colorscheme
 
 -- Change value of colorscheme = "palletes" depending on the set value of the widget
 local function set_theme(text,line,path)
@@ -35,24 +35,21 @@ local function set_theme(text,line,path)
 
 end
 
--- Widgets
-----------
-local currTheme   = user.colorscheme
-
 local drawing     = wibox.widget {
-  image = gears.surface.load_uncached(preview_image .. string.lower(currTheme) .. '.png'),
-  opacity = 0.30,
-  align       = 'center',
-  widget = wibox.widget.imagebox
+  resize = true,
+  forced_width = 480,
+  forced_height = 250,
+  shape = helpers.rounded_rect(3),
+  widget = wibox.widget.imagebox,
+  horizontal_fit_policy = "fit",
+  vertical_fit_policy = "fit",
+  image = gears.surface.load_uncached(preview_image .. string.lower(currTheme) .. '.png')
 }
 local name        = wibox.widget {
   markup = currTheme,
-  font   = beautiful.font_sans .. ' 12',
-  align = 'right',
+  font   = beautiful.font_sans .. " 12",
   widget = wibox.widget.textbox
 }
-
-
 local themes      = {
   'everblush',
   'everforest',
@@ -81,87 +78,103 @@ end
 
 local pos         = helpers.indexOf(themes, currTheme)
 
-local prev_button = wibox.widget {
-  markup = '󰅁',
-  font   = beautiful.font_icon .. ' 18',
-  widget = wibox.widget.textbox
-}
-
-prev_button:buttons(gears.table.join(awful.button({}, 1, function()
-  if pos > 1 then
-    pos = pos - 1
-    currTheme = themes[pos]
-    drawing.image = gears.surface.load_uncached(preview_image .. string.lower(currTheme) .. '.png')
-    name.markup = currTheme
-  end
-end)))
-
-local next_button = wibox.widget {
-  markup = '󰅂',
-  font   = beautiful.font_icon .. ' 18',
-  widget = wibox.widget.textbox
-}
-
-next_button:buttons(gears.table.join(awful.button({}, 1, function()
-  if pos < #themes then
-    pos = pos + 1
-    currTheme = themes[pos]
-    drawing.image =  gears.surface.load_uncached(preview_image .. string.lower(currTheme) .. '.png')
-    name.markup = currTheme
-  end
-end)))
-
-local set_theme_button = wibox.widget {
-  markup = 'Set Theme',
-  font   = beautiful.font_sans .. ' 12',
-  widget = wibox.widget.textbox
-}
-
-set_theme_button:buttons(gears.table.join(awful.button({}, 1, function()
-  set_theme('   colorscheme = "' .. currTheme:gsub('"', '\\"') .. '",',21,gfs.get_configuration_dir() .. "config/user.lua") --Change theme
-  -- set_theme("include " .. currTheme .. ".ini",3,os.getenv("HOME") .. "/.config/kitty/kitty.conf") --Change terminal color (kitty)
-  -- set_theme('@import "'..currTheme:gsub('"', '\\"') .. '.rasi"',1,os.getenv("HOME") .. "/.config/rofi/appmnu.rasi") --Change rofi color
-end)))
-
-
-local buttons_layout = wibox.widget {
-  prev_button,
-  next_button,
-  set_theme_button,
-  spacing = 10,
-  layout = wibox.layout.fixed.horizontal
-}
-
-local buttons_with_padding = wibox.container.margin(buttons_layout, 218, 10, 0, 0)
-local name_with_padding = wibox.container.margin(name, 240, 10, 10, 0)
-
-local name_and_buttons_layout = wibox.widget {
-  name_with_padding,
-  buttons_with_padding,
-  spacing = 180,
-  layout = wibox.layout.fixed.vertical
-}
-
-local image_and_name_and_buttons_layout = wibox.widget {
-  drawing,
-  name_and_buttons_layout,
-  layout = wibox.layout.stack
-}
-
 local final_widget = wibox.widget {
-  image_and_name_and_buttons_layout,
-  bg = {
-    type = 'linear',
-    from = { 0, 0 },
-    to = { 500, 0 },
-    stops = { { 0, beautiful.bg_light .. '31' }, { 1, beautiful.bg_normal } }
+  {
+    {
+      {
+        drawing,
+        {
+          {
+            widget = wibox.widget.textbox,
+          },
+          bg = {
+            type = "linear",
+            from = { 0, 0 },
+            to = { 335, 0 },
+            stops = { { 0, beautiful.bg_normal .. "31" }, { 1, beautiful.bg_normal } }
+          },
+          widget = wibox.container.background,
+        },
+        {
+          {
+            {
+              {
+                {
+                  markup = "󰅁",
+                  font   = beautiful.font_icon .. " 18",
+                  widget = wibox.widget.textbox
+                },
+                widget = wibox.container.margin,
+                buttons = awful.button({}, 1, function()
+                  if pos > 1 then
+                    pos = pos - 1
+                    currTheme = themes[pos]
+                    drawing.image = gears.surface.load_uncached(preview_image .. string.lower(currTheme) .. '.png')
+                    name.markup = currTheme
+                  end
+                end),
+              },
+              {
+                {
+                  markup = "󰅂",
+                  font   = beautiful.font_icon .. " 18",
+                  widget = wibox.widget.textbox
+                },
+                widget = wibox.container.margin,
+                buttons = awful.button({}, 1, function()
+                  if pos < #themes then
+                    pos = pos + 1
+                    currTheme = themes[pos]
+                    drawing.image = gears.surface.load_uncached(preview_image .. string.lower(currTheme) .. '.png')
+                    name.markup = currTheme
+                  end
+                end),
+              },
+              {
+                {
+                  markup = "Set Theme",
+                  font   = beautiful.font_sans .. " 12",
+                  widget = wibox.widget.textbox
+                },
+                widget = wibox.container.margin,
+                buttons = awful.button({}, 1, function()
+                  set_theme('   colorscheme = "' .. currTheme:gsub('"', '\\"') .. '",',21,gfs.get_configuration_dir() .. "config/user.lua") --Change theme
+                  -- set_theme("include " .. currTheme .. ".ini",3,os.getenv("HOME") .. "/.config/kitty/kitty.conf") --Change terminal color (kitty)
+                  -- set_theme('@import "'..currTheme:gsub('"', '\\"') .. '.rasi"',1,os.getenv("HOME") .. "/.config/rofi/appmnu.rasi") --Change rofi color
+                end),
+              },
+              spacing = 10,
+              layout = wibox.layout.fixed.horizontal
+            },
+            widget = wibox.container.place,
+            halign = 'right',
+            valign = 'bottom',
+          },
+          widget = wibox.container.margin,
+          bottom = 10,
+          right = 10,
+        },
+        {
+          {
+            name,
+            widget = wibox.container.place,
+            halign = 'right',
+            valign = 'top',
+          },
+          widget = wibox.container.margin,
+          top = 10,
+          right = 10,
+        },
+        widget = wibox.layout.stack
+      },
+      spacing = 15,
+      layout = wibox.layout.fixed.vertical
+    },
+    widget = wibox.container.margin,
   },
   widget = wibox.container.background,
-  forced_width = 250,
-  forced_height = 250,
-  shape = helpers.rounded_rect(dpi(4))
+  bg = beautiful.bg_normal .. 'cc',
 }
-
 
 return {
   widget = final_widget,
