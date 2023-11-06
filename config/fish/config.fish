eee812db0c5424db9cc6fa28d32728e9c6e0df17
osmarmora05 <osmaradrianmoracerna@gmail.com>
2023-11-06 14:47:21 -0600

    chore: add fish config

---
+++ config/fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    # colorscript random
+    # pokemon-colorscripts -r
+    # pokemon-colorscripts -r --no-title
+    starship init fish | source	
+end
---
+++ config/fish/functions/fish_prompt.fish
@@ -0,0 +1,14 @@
+function fish_prompt
+    if test -n "$SSH_TTY"
+        echo -n (set_color brred)"$USER"(set_color white)'@'(set_color yellow)(prompt_hostname)' '
+    end
+
+    echo -n (set_color blue)(prompt_pwd)' '
+
+    set_color -o
+    if fish_is_root_user
+        echo -n (set_color green)'# '
+    end
+    echo -n (set_color green)'󰊠  '
+    set_color normal
+end
