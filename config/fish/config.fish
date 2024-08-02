cdbda5abddc00d11996b29d5b0078c9fbeea2697
osmarmora05 <101274294+osmarmora05@users.noreply.github.com>
2024-08-02 10:27:35 -0600

    chrore: add oh-my-posh in fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,7 +1,5 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    # colorscript random
-    # pokemon-colorscripts -r
-    # pokemon-colorscripts -r --no-title
-    starship init fish | source	
+    # starship init fish | source
+    oh-my-posh init fish --config "~/.config/oh-my-posh.toml" | source
 end
